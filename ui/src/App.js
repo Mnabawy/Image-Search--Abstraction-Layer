11316773e00f57e9e77ee2cab3067e0bf37f8770
Mnabawy <m_ninja61@yahoo.com>
2021-09-29 13:07:28 +0200

    initial

---
+++ ui/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import './App.css';
+import Images from './components/images';
+
+
+
+
+class App extends React.Component {
+  state = {
+    url: "",
+  }
+
+  handleCallBack = (childData) => {
+    this.setState({ url: childData })
+  }
+
+  componentDidMount() {
+    this.handleCallBack()
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>Image Search Abstraction Layer</h1>
+        <hr />
+        <p>Search the web for images or see recent searches</p>
+
+        <Images parentCallback={this.handleCallBack} />
+
+      </div>
+    );
+  }
+}
+
+export default App;
